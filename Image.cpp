a29effe08307fbfd810577a57ee85e1997452dc7
Brian Tilley <briantilley97@gmail.com>
2016-10-10 13:55:28 -0500

    adding methods to Image

--- Image.cpp
+++ Image.cpp
@@ -4,25 +4,26 @@
 #include "Image.h"
 
 // helper to compute indices
-inline unsigned Image::linearIndex(unsigned column, unsigned row) {
-	return row * this->width + column;
+inline unsigned Image::linearIndex(unsigned column, unsigned row) const {
+	return row * width() + column;
 }
 
-Image::Image(): values(nullptr), width(0), height(0), coords(nullptr) { }
+Image::Image(): m_values(nullptr), m_width(0), m_height(0), m_coords(nullptr) { }
 
-Image::Image(unsigned width, unsigned height, float* values, float* coords): width(width), height(height) {
-	this->values = new float[width * height];
-	this->coords = new float[width * height * 2];
+Image::Image(unsigned width, unsigned height, const float* values, const float* coords): m_width(width), m_height(height) {
+	// allocate space even for blank images
+	m_values = new float[width * height];
+	m_coords = new float[width * height * 2];
 
 	// either copy or fill as blank
 	if(values != nullptr)
-		memcpy(this->values, values, sizeof(float) * width * height);
+		memcpy(m_values, values, sizeof(float) * width * height);
 	else
-		memset(this->values, 0, sizeof(float) * width * height);
+		memset(m_values, 0, sizeof(float) * width * height);
 
 	// either copy or fill with default coordinates matching image coordinates
 	if(coords != nullptr)
-		memcpy(this->coords, coords, sizeof(float) * width * height * 2);
+		memcpy(m_coords, coords, sizeof(float) * width * height * 2);
 	else {
 		float xCoords[width];
 
@@ -32,8 +33,8 @@ Image::Image(unsigned width, unsigned height, float* values, float* coords): wid
 				float xCoord = static_cast<float>(col) / width;
 				xCoords[col] = xCoord;
 
-				this->coords[linearIndex(2 * col, 0)] = xCoord;
-				this->coords[linearIndex(2 * col + 1, 0)] = 0.f;
+				m_coords[linearIndex(2 * col, 0)] = xCoord;
+				m_coords[linearIndex(2 * col + 1, 0)] = 0.f;
 		}
 
 		// fill all subsequent rows using recall for x coords
@@ -43,19 +44,51 @@ Image::Image(unsigned width, unsigned height, float* values, float* coords): wid
 			for(int col = 0; col < width; ++col) {
 				float xCoord = static_cast<float>(col) / width;
 
-				this->coords[linearIndex(2 * col, row)] = xCoords[col];
-				this->coords[linearIndex(2 * col + 1, 0)] = yCoord;
+				m_coords[linearIndex(2 * col, row)] = xCoords[col];
+				m_coords[linearIndex(2 * col + 1, 0)] = yCoord;
 			}
 		}
 	}
 }
 
-Image::Image(const Image& arg) {
-
-}
+Image::Image(const Image& arg): Image(arg.width(), arg.height(), arg.m_values, arg.m_coords) { }
 
 Image::~Image() {
 	// free heap memory
-	delete this.values;
-	delete this.coords;
+	delete m_values;
+	delete m_coords;
+}
+
+Pixel Image::at(unsigned col, unsigned row) const {
+	return
+	{
+		m_values[linearIndex(col, row)],
+		{ m_coords[linearIndex(col * 2, row)], m_coords[linearIndex(col * 2 + 1, row)] }
+	};
+}
+
+Image Image::resample(unsigned width, unsigned height) const {
+	Image result(width, height);
+
+
+	return result;
+}
+
+void Image::shiftCorners(Coordinate upRight, Coordinate upLeft, Coordinate downRight, Coordinate downLeft) {
+
+}
+
+void Image::spatialScale() {
+
+}
+
+void Image::spatialNormalize() {
+
+}
+
+Image Image::rasterize() const {
+	Image result(width(), height());
+
+
+	return result;
 }
--- Image.h
+++ Image.h
@@ -13,30 +13,32 @@ struct Pixel {
 // manage only float values and give each pixel its own coordinates
 class Image {
 private:
-	float* values; // grayscale values from 0.0 to 1.0
-	const unsigned width, height; // image coordinates
-	float* coords; // pixel positions (interleaved as xyxyxyxyxy)
+	float* m_values; // grayscale values from 0.0 to 1.0
+	const unsigned m_width, m_height; // image coordinates
+	float* m_coords; // pixel positions (interleaved as xyxyxyxyxy)
 
 	// make accesses easier
-	unsigned linearIndex(unsigned column, unsigned row);
+	unsigned linearIndex(unsigned column, unsigned row) const;
 
 public:
 	Image();
-	Image(unsigned width, unsigned height, float* values = nullptr, float* coords = nullptr);
+	Image(unsigned width, unsigned height, const float* values = nullptr, const float* coords = nullptr);
 	Image(const Image& arg);
 
 	// make sure to delete dynamic memory
 	~Image();
 
 	// all data of a pixel at a certain image coordinate
-	Coordinate at(unsigned column, unsigned row);
+	Pixel at(unsigned column, unsigned row) const;
+	unsigned width() const { return m_width; }
+	unsigned height() const { return m_height; }
 
-	Image resample(unsigned width, unsigned height);
+	Image resample(unsigned width, unsigned height) const;
 	void shiftCorners(Coordinate upRight, Coordinate upLeft,
 		Coordinate downRight, Coordinate downLeft);
 	void spatialScale();
 	void spatialNormalize();
-	Image rasterize();
+	Image rasterize() const;
 };
 
 #endif
